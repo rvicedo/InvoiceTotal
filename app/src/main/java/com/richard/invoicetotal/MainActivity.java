e7d674b8f4d60b74af315a919f4202d65effdb96
rvicedo <richardvicedo@gmail.com>
2016-01-11 21:09:32 -0800

    comment

--- app/src/main/java/com/richard/invoicetotal/MainActivity.java
+++ app/src/main/java/com/richard/invoicetotal/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    //comment
+    //commen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
